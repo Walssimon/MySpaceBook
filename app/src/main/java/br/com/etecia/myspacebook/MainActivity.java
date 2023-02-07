aa79c89636dbc4c6cf6703177bce049674857720
walssimon <walssimon123@gmail.com>
2023-02-07 20:25:51 -0300

    initial commit

--- app/src/main/java/br/com/etecia/myspacebook/MainActivity.java
+++ app/src/main/java/br/com/etecia/myspacebook/MainActivity.java
@@ -3,12 +3,17 @@ package br.com.etecia.myspacebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView idlistaValores;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
