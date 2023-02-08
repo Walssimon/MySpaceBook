91a3ebca8f58740278fc8328adbbba6fabdfd96c
walssimon <walssimon123@gmail.com>
2023-02-07 21:24:03 -0300

    Adapter  commit

--- app/src/main/java/br/com/etecia/myspacebook/MainActivity.java
+++ app/src/main/java/br/com/etecia/myspacebook/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.etecia.myspacebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +17,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idlistaValores = findViewById(R.id.listaValores);
+        myadapter adapter = new myadapter();
+
+        idlistaValores.setAdapter(adapter);
+    }
+    public class myadapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 
 }
